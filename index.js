892ce1b5b439f68536a4cd3e6d9c5d0a732d03c1
Archie Baer <archie@abaer.dev>
2021-01-01 18:38:50 +0000

    No longer changed MOTD

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 // Create Server
 const mc = require("minecraft-protocol");
-const server = mc.createServer({ maxPlayers: 1 });
-
-// Update Server Info
-const s = "\u00a7";
-const motd = [
-  `3Follow ${s}b@alleshq${s}3 on Twitter`,
-  `2This server runs on renewable energy!`,
-  `7Made with ${s}4\u2764 ${s}7by ${s}9Alles`,
-  `9https://mcauth.alles.cx`,
-];
-let m = 0;
-const updateInfo = () => {
-  if (m >= motd.length - 1) m = 0;
-  else m++;
-  server.motd = `${s}c\u21d2 Join to get an auth code\n${s}${motd[m]}`;
-};
-setInterval(updateInfo, 1000);
-updateInfo();
+const server = mc.createServer({
+  motd:
+    "\u00a7c\u21d2 Join to get an auth code\n" +
+    "\u00a77Made with \u00a74\u2764 \u00a77by \u00a79Alles",
+  maxPlayers: 1,
+});
 
 // Player Login
 server.on("login", (client) => {
