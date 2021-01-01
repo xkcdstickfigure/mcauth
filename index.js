3160b71f35f93e23055f68a7ded65325eff9f663
Archie Baer <archie@abaer.dev>
2021-01-01 20:21:17 +0000

    Stores tokens in database

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+require("dotenv").config();
+
+// Database
+const Sequelize = require("sequelize");
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mariadb",
+    logging: false,
+  }
+);
+const Token = db.define(
+  "token",
+  {
+    id: {
+      primaryKey: true,
+      type: Sequelize.UUID,
+      allowNull: false,
+    },
+    token: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    player: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  { updatedAt: false }
+);
+db.sync();
+
 // Create Server
 const mc = require("minecraft-protocol");
 const server = mc.createServer({
@@ -8,10 +43,22 @@ const server = mc.createServer({
 });
 
 // Player Login
-server.on("login", (client) => {
-  console.log({
-    id: client.profile.id,
-    username: client.profile.name,
+const { v4: uuid } = require("uuid");
+const { generate: random } = require("randomstring");
+server.on("login", async (client) => {
+  // Generate Token
+  const token = await Token.create({
+    id: uuid(),
+    token: random({
+      length: 7,
+      readable: true,
+      capitalization: "uppercase",
+    }),
+    player: client.profile.id,
   });
-  client.end("test");
+  client.end(
+    `\u00a77Your token is \u00a7a${token.token}\n\n` +
+      `\u00a7cDo not enter this anywhere other than Alles.\n` +
+      `Expires in 5 minutes.`
+  );
 });
